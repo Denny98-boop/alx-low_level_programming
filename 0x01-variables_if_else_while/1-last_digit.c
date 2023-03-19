b208be6b9c73716543635f768c9744a2fa3cfb1a
Denny98-boop <mwangidennys21@gmail.com>
2023-03-19 03:53:42 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: Always 0 (Sucess/correct)
  */
@@ -18,7 +18,7 @@ n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
 {
-printf("Last digit of $i is %i and is greater than 5\n", n, n % 10);
+printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 }
 
 else if (n % 10 > == 0)
