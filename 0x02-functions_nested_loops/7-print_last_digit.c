7d5ac00744e9abf807513442818f4d9c171b572c
Denny98-boop <mwangidennys21@gmail.com>
2023-03-21 14:14:30 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,13 @@ int print_last_digit(int n)
 
 {
 
-	int last;
+	int last_digit= n % 10;
 
-	last = n % 10;
-	if (last < 0)
-	{
-	last = last * -1;
-	}
-	_putchar(last * '0');
-	return (last);
+	if  (last_digit < 0)
+	last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 
 }
