473709d1732a823c152d4287975e1124ff5a9a3f
Denny98-boop <mwangidennys21@gmail.com>
2023-03-21 08:14:06 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - make alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <+ 0)
+	{
+	for (c = 'a'; c <= '2'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+
+}
