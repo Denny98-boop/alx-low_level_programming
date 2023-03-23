9b49487521d9451fa1a166908f2a7ed4df353912
Denny98-boop <mwangidennys21@gmail.com>
2023-03-23 14:17:39 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests functions that printsif integeris positive or negative
+ * Returns: 0
+ */
+int main(void)
+
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+
+}
