88e781dcf510a0da845e1f3357fd45a04dc36db6
Denny98-boop <mwangidennys21@gmail.com>
2023-03-23 15:32:22 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,16 +14,16 @@ int largest;
 
 if (a > b && b > c)
 {
-        largest = a;
+largest = a;
 }
 else if (b > a && a > c)
 {
-        largest = b;
+largest = b;
 }
 else
 {
-        largest = c;
+largest = c;
 }
 
-    return (largest);
+return (largest);
 }
