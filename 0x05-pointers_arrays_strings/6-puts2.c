c2cde4f40957d61973cdb94bc36f7ea6d8ad5b00
Denny98-boop <mwangidennys21@gmail.com>
2023-03-27 14:02:52 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 {
 int a;
 
-for (a = 0; str[a] '\0' ; a++)
+for (a = 0; str[a] != '\0'; a++)
 {
 if (a % 2 == 0)
 _putchar (str[a]);
 }
 
--putchar ('\n');
+_putchar ('\n');
 }
