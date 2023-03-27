b2841832c7dbb3fd20e0a66eef9397a17060c7cd
Denny98-boop <mwangidennys21@gmail.com>
2023-03-27 13:07:47 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * rev_string - print a reverse string
+ * @s: A pointer to an integer that will be changed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+char *start_c, end_c;
+int i, count;
+int length = 0;
+
+for (i = 0; s[i]; i++)
+{
+length++;
+}
+
+count = length;
+
+start_c =s;
+end_c = s;
+
+
+for (i = 0; i < count - 1; i++)
+{
+end_c++;
+}
+
+
+for (i = 0; i < count / 2; i++)
+{
+
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+
+
+start_c++;
+end_c--;
+}
+}
