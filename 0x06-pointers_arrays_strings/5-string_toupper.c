1d7173b7559891f7ae9f84951219e00a89383784
Denny98-boop <mwangidennys21@gmail.com>
2023-03-30 10:48:09 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *string_toupper - change all lowercases letters of a string to uppercase
+ *@x: pointer to string
+ *Return: pointer to uppercase string
+ */
+char *string_toupper(char *x)
+
+{
+int length_of_string;
+
+length_of_string = 0;
+
+while (x[length_of_string] != '\0')
+{
+if (x[length_of_string] >= 97 && x[length_of_string] <= 122)
+{
+x[length_of_string] = x[length_of_string] - 32;
+}
+length_of_string++;
+}
+return (x);
+}
+
